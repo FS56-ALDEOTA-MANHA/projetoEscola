ee8fa36831bdd68017f7de3d0fc5e873473caa1a
nayaracalenzo <queiroz.14@hotmail.com>
2026-02-11 11:57:42 -0300

    carrossel customizado

--- script/dashboard.js
+++ script/dashboard.js
@@ -31,6 +31,14 @@
     sede: "Sul",
     img: "https://digitalcollege.com.br/wp-content/uploads/2024/01/sou-colleger.png",
   },
+  {
+    id: 4,
+    nome: "Escalone suas aplicações",
+    data: "13/02/2026",
+    horario: "18:00",
+    sede: "Sul",
+    img: "https://digitalcollege.com.br/wp-content/uploads/2024/01/sou-colleger.png",
+  },
   
 ]
 
@@ -39,8 +47,18 @@ let carrossel = document.getElementById('carrossel')
 
 for (let i = 0; i < cursosPlus.length; i++) {
   carrossel.innerHTML +=  `
-  <div id="slide${cursosPlus[i].id}" class="carousel-item relative w-full">
-    <img src=${cursosPlus[i].img} class="w-full" />
+  <div id="slide${cursosPlus[i].id}" class="carousel-item relative w-full flex justify-between">
+    <div class="w-1/2 flex flex-col justify-center pl-50">
+    <div class="flex-1 pt-20">
+      <h1 class="font-bold text-4xl text-rose-500 pb-3">${cursosPlus[i].nome}</h1>
+      <div>
+        <span>${cursosPlus[i].data}</span>
+        <span>${cursosPlus[i].horario}</span>
+      </div>
+      <span class="font-bold">Digital College ${cursosPlus[i].sede}</span>
+      </div>
+    </div>
+    <img src=${cursosPlus[i].img} class="w-1/2 w-[600px] h-[400px] pr-50" />
     <div class="absolute left-5 right-5 top-1/2 flex -translate-y-1/2 transform         justify-between">
       <a href="#slide${cursosPlus[i].id - 1}" class="btn btn-circle">❮</a>
       <a href="#slide${i === cursosPlus.length - 1 ? cursosPlus[0].id:cursosPlus[i].id + 1}" class="btn btn-circle">❯</a>
